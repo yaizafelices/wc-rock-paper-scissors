6bc30440e683f47ee70f4c7eb5ec52c7fd4ae965
Yaiza Soria Felices <yaiza.soria@kairosds.com>
2022-12-15 13:13:53 +0100

    feat:create ramdom letter

--- src/GameRockpaperscissors.js
+++ src/GameRockpaperscissors.js
@@ -1,37 +1,125 @@
-import { html, css, LitElement } from 'lit';
+import { html, LitElement } from 'lit';
+import { RockPaperScissorsStyles } from'./styles-wc-rock-paper-scissors.js';
 
 export class GameRockpaperscissors extends LitElement {
-  static get styles() {
-    return css`
-      :host {
-        display: block;
-        padding: 25px;
-        color: var(--game-rockpaperscissors-text-color, #000);
-      }
-    `;
-  }
-
   static get properties() {
-    return {
-      title: { type: String },
-      counter: { type: Number },
-    };
+    return  {
+      position: { type: Object },
+      diameter: { type: String },
+      type: { type: String },
+      letter: { type: Array },
+      item: { type: Array },
+      cycle: { type: Object },
+      memory: { type: Array }
+  }
+};
+
+  static get styles() {
+    return [RockPaperScissorsStyles];
   }
 
-  constructor() {
-    super();
-    this.title = 'Hey there';
-    this.counter = 5;
-  }
+ constructor() {
+  super();
+  this._maxWidth = 600;
+  this._maxHeight = 600;
+  this.id = `wccell-${this.randomNum(1, 1000)}-${  new Date().getTime()}`;
+  this.position = {
+    top: `${this.randomNum(this._maxRadius, this._maxHeight - this._maxRadius)}px`,
+    left: `${this.randomNum(this._maxRadius, this._maxWidth - this._maxRadius)}px`
+  };
+  this.diameter = '10px';
+  this.type = this.randomNum(0, 3);
+  
 
-  __increment() {
-    this.counter += 1;
-  }
+  this.letter = ['R', 'P', 'S'];
 
-  render() {
-    return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
-    `;
+  this.item = this.randomLetter(this.letter);
+
+  this._moveTime = 300;
+  this.memory = [];
+
+  this.move = this.move.bind(this);
+
+}
+
+move() {
+  const newTop = parseInt(this.position.top, 10) + this.randomNum(-10, 10);
+  const newLeft = parseInt(this.position.left, 10) + this.randomNum(-10, 10);
+  if (newTop >= 0 && newTop <= this._maxHeight - this._maxRadius) {
+    this.position.top = `${newTop}px`;
   }
+  if (newLeft >= 0 && newLeft <= this._maxWidth - this._maxRadius) {
+    this.position.left = `${newLeft}px`;
+  }
+  this._livingWccellMoveEvent();
+  this._setStyles();
+}
+
+_livingWccellMoveEvent() {
+  document.dispatchEvent(
+    new CustomEvent('living-wccell-move', {
+      detail: {
+        id: this.id,
+        position: this.position,
+        diameter: this.diameter,
+        letter: this.letter
+      }
+    })
+  );
+}
+
+_setStyles() {
+  const styles = this.shadowRoot.querySelector('.cell').style;
+  styles.top = `${this.position.top}`;
+  styles.left = `${this.position.left}`;
+  styles.width = this.diameter;
+  styles.height = this.diameter;
+  styles.backgroundColor = `hsl(${this.color * 10}, 100%, 50%)`;
+  if (this.age === this.cycle.life - 1) {
+    styles.animationName = 'death' ;
+  }
+}
+
+_calculateDistance(e) {
+  const x = parseInt(this.position.top, 10) - parseInt(e.detail.position.top, 10);
+  const y = parseInt(this.position.left, 10) - parseInt(e.detail.position.left, 10);
+  // console.log(this.position, e.detail.position);
+  return Math.sqrt(x * x + y * y);
+}
+
+_insertCell(e) {
+  const idParts = this.id.split('-');
+  const idPartsDetail = e.detail.id.split('-');
+  const id = `${idParts[0]}-${idParts[1]+idPartsDetail[1]}-${new Date().getTime()}`;
+  console.log(`created cell with id ${id}`);
+  if (!document.getElementById(id)) {
+    const rockpaperscissors = `<game-rockpaperscissors id="${id}"></game-rockpaperscissors>`;
+    document.body.insertAdjacentHTML('beforeend', rockpaperscissors);
+    setTimeout(() => {
+      const newCell = document.getElementById(id);
+      if (newCell) {
+        newCell.position = {
+          top: `${parseInt(this.position.top, 10) + 50}px`,
+          left: `${parseInt(this.position.left, 10) + 50}px`
+        };
+      }
+    }, 100);
+  }
+}
+
+randomNum(min, max) {
+  this._null = null;
+  return parseInt(Math.random() * (max + 1 - min), 10) + min;
+}
+
+randomLetter() {
+  return this.letter[this.randomNum(0, 2)];
+}
+
+render() {
+  return html`
+    <div class="cell">${this.item}</div>
+  `;
+}
+
 }
---
+++ src/styles-wc-rock-paper-scissors.js
@@ -0,0 +1,22 @@
+import { css } from 'lit';
+
+export const RockPaperScissorsStyles = css `
+:host {
+    display: block;
+  }
+.cell {
+    position: absolute;
+    border-radius: 50%;
+    background-color: plum;
+    cursor: pointer;
+    animation-duration: 2s;
+    animation-name: show;
+    opacity: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: black;
+  }
+
+  
+`;
